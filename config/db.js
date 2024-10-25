4295ae1d2b2f00797055a1bdba55cc173e4fb2eb
JuanRG-20221039 <20221039@uthh.edu.mx>
2024-10-24 23:19:27 -0600

    modified:   config/db.js

--- config/db.js
+++ config/db.js
@@ -1,30 +1,10 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//     try {
-//         const mongoURI = "mongodb+srv://20221039:eKvvP6ckAX06Uyae@prueba.afe17.mongodb.net/paulo_freiredb?retryWrites=true&w=majority&appName=PRUEBA";
-        
-//         const conn = await mongoose.connect(mongoURI);
-//         const url = `${conn.connection.host}:${conn.connection.port}`;
-//         console.log(`MongoDB connected: ${url} ✅`);
-//     } catch (error) {
-//         console.log(`Error: ${error.message}`);
-//         process.exit(1);
-//     }
-// };
-
-// export default connectDB;
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const mongoURI = "mongodb://localhost:27017/paulo_freiredb"; // URI para la base de datos local
+        const mongoURI = "mongodb+srv://20221039:eKvvP6ckAX06Uyae@prueba.afe17.mongodb.net/paulo_freiredb?retryWrites=true&w=majority&appName=PRUEBA";
         
-        const conn = await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(mongoURI);
         const url = `${conn.connection.host}:${conn.connection.port}`;
         console.log(`MongoDB connected: ${url} ✅`);
     } catch (error) {
@@ -34,3 +14,27 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
+
+
+
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//     try {
+//         const mongoURI = "mongodb://localhost:27017/paulo_freiredb"; // URI para la base de datos local
+        
+//         const conn = await mongoose.connect(mongoURI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         const url = `${conn.connection.host}:${conn.connection.port}`;
+//         console.log(`MongoDB connected: ${url} ✅`);
+//     } catch (error) {
+//         console.log(`Error: ${error.message}`);
+//         process.exit(1);
+//     }
+// };
+
+// export default connectDB;
