08dc4bc4f0148199ca09253959cd9c9f81c5de0e
JuanRG-20221039 <20221039@uthh.edu.mx>
2024-11-18 17:27:10 -0600

    modified:   config/db.js

--- config/db.js
+++ config/db.js
@@ -1,24 +1,24 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const connectDB = async () => {
-//     try {
-//         const mongoURI = "mongodb+srv://20221039:eKvvP6ckAX06Uyae@prueba.afe17.mongodb.net/paulo_freiredb?retryWrites=true&w=majority&appName=PRUEBA";
+const connectDB = async () => {
+    try {
+        const mongoURI = "mongodb+srv://20221039:eKvvP6ckAX06Uyae@prueba.afe17.mongodb.net/paulo_freiredb?retryWrites=true&w=majority&appName=PRUEBA";
         
-//         const conn = await mongoose.connect(mongoURI);
-//         const url = `${conn.connection.host}:${conn.connection.port}`;
-//         console.log(`MongoDB connected: ${url} ✅`);
-//     } catch (error) {
-//         console.log(`Error: ${error.message}`);
-//         process.exit(1);
-//     }
-// };
+        const conn = await mongoose.connect(mongoURI);
+        const url = `${conn.connection.host}:${conn.connection.port}`;
+        console.log(`MongoDB connected: ${url} ✅`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
 
-// export default connectDB;
-
-
+export default connectDB;
 
 
 
+
+/*
 import mongoose from "mongoose";
 
 const connectDB = async () => {
@@ -38,3 +38,4 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+*/
